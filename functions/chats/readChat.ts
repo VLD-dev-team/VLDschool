d10b5f9971d7c3dd8c05abf35185ebe85ce1cfdb
Vrock691 <vrock691@gmail.com>
2024-10-07 23:02:09 +0200

    feat: readChat fonction implémentée

---
+++ functions/chats/readChat.ts
@@ -0,0 +1,17 @@
+"use server";
+
+import executeQuery from "@/db";
+
+export default async function readChat(chatID: number, userID: number): Promise<boolean> {
+
+    try {
+        
+        const query = `INSERT INTO chatreadedby ("chatID", "userID") VALUES ($1, $2);`
+        var results = await executeQuery(query, [chatID, userID]);
+        return true;
+
+    } catch (error) {
+        throw error
+    }
+
+}
